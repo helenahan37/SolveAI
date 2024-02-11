6b430d9c9be42c03e79ede68b4df6a91554ab9cb
Helena.Han <hlyhelena37@gmail.com>
2024-02-11 13:14:16 +1100

    config: react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Home = () => {
+	return <h1>Home</h1>;
+};
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,11 +15,9 @@ root.render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
