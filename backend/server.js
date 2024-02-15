af4934d11aa9e3fd6e647a65f4801b61ef96a608
Helena.Han <hlyhelena37@gmail.com>
2024-02-15 18:06:29 +1100

    update cors option

--- backend/server.js
+++ backend/server.js
@@ -101,7 +101,7 @@ app.use(cookieParser());
 
 //*===CORS===//
 const corsOptions = {
-	origin: 'solveai.netlify.app' || 'http://localhost:3000',
+	origin: 'https://solveai.netlify.app' || 'http://localhost:3000',
 	credentials: true,
 };
 app.use(cors(corsOptions));
