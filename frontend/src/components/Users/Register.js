5c06e305e8f9c3719d11fe3bfa5fb1401b1541eb
Helena.Han <hlyhelena37@gmail.com>
2024-02-16 23:51:24 +1100

    Update Register.js

--- frontend/src/components/Users/Register.js
+++ frontend/src/components/Users/Register.js
@@ -25,7 +25,7 @@ const Registration = () => {
 		if (isAuthenticated) {
 			navigate('/dashboard');
 		}
-	}, [isAuthenticated]);
+	}, [isAuthenticated, login]);
 
 	const mutation = useMutation({ mutationFn: registerAPI });
 	// Formik setup for form handling
