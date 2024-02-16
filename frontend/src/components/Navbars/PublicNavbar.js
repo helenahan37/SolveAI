d11e78d64fe278224c8bb58b45ae11a7467ab371
Helena.Han <hlyhelena37@gmail.com>
2024-02-17 01:06:44 +1100

    Update PublicNavbar.js

--- frontend/src/components/Navbars/PublicNavbar.js
+++ frontend/src/components/Navbars/PublicNavbar.js
@@ -36,9 +36,9 @@ export default function PublicNavbar() {
 						</div>
 						<div className="hidden lg:flex lg:gap-x-12">
 							{navigation.map((item) => (
-								<Link key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
+								<a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white">
 									{item.name}
-								</Link>
+								</a>
 							))}
 						</div>
 						<div className="hidden lg:flex lg:flex-1 lg:justify-end">
