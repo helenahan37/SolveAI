5798f8d8fea957305aedaa00ec260315327195a2
Helena.Han <hlyhelena37@gmail.com>
2024-02-17 11:54:12 +1100

    update cookie

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -71,7 +71,7 @@ const login = asyncHandler(async (req, res) => {
 	res.cookie('token', token, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === 'production',
-		sameSite: 'strict',
+		sameSite: 'none',
 		expires: 7 * 24 * 60 * 60 * 1000,
 	});
 
