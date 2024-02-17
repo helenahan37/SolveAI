78fb180f122333c4b4ea7d1140883391e2c573a7
Helena.Han <hlyhelena37@gmail.com>
2024-02-17 15:02:28 +1100

    update domain

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -73,6 +73,7 @@ const login = asyncHandler(async (req, res) => {
 		secure: process.env.NODE_ENV === 'production',
 		sameSite: 'none',
 		expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+		domain: 'solveai.netlify.app'
 	});
 
 	//send response
