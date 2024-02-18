321ba62daeafcebe639590fafb1fb4f9398df849
Helena.Han <hlyhelena37@gmail.com>
2024-02-18 22:04:26 +1100

    update usersapi

--- frontend/src/apis/user/usersAPI.js
+++ frontend/src/apis/user/usersAPI.js
@@ -3,44 +3,26 @@ import baseURL from '../../utils';
 
 //* =============User Registration=========
 export const registerAPI = async (userData) => {
-	const response = await axios.post(
-		`${baseURL}/users/register`,
-		{
-			username: userData?.username,
-			email: userData?.email,
-			password: userData?.password,
-		},
-		{
-			withCredentials: true,
-		}
-	);
+	const response = await axios.post(`${baseURL}/users/register`, {
+		username: userData?.username,
+		email: userData?.email,
+		password: userData?.password,
+	});
 	return response?.data;
 };
 
-//* =============User login=========
+//* =====User login=========
 export const loginAPI = async (userData) => {
-	const response = await axios.post(
-		`${baseURL}/users/login`,
-		{
-			email: userData?.email,
-			password: userData?.password,
-		},
-		{
-			withCredentials: true,
-		}
-	);
+	const response = await axios.post(`${baseURL}/users/login`, {
+		email: userData?.email,
+		password: userData?.password,
+	});
 	return response?.data;
 };
 
 //* =============Check Auth=========
 export const checkAuthAPI = async () => {
-	const response = await axios.get(
-		`${baseURL}/users/auth/check`,
-
-		{
-			withCredentials: true,
-		}
-	);
+	const response = await axios.get(`${baseURL}/users/auth/check`);
 	return response?.data;
 };
 
@@ -58,25 +40,13 @@ export const logoutAPI = async () => {
 
 //* ==========Get UserProfile=========
 export const getUserProfileAPI = async () => {
-	const response = await axios.get(
-		`${baseURL}/users/profile`,
-
-		{
-			withCredentials: true,
-		}
-	);
+	const response = await axios.get(`${baseURL}/users/profile`);
 	return response?.data;
 };
 
 //* ==========Delete ContentHistory=========
 
 export const deleteContentAPI = async (contentId) => {
-	const response = await axios.delete(
-		`${baseURL}/users/content-delete/${contentId}`,
-
-		{
-			withCredentials: true,
-		}
-	);
+	const response = await axios.delete(`${baseURL}/users/content-delete/${contentId}`);
 	return response?.data;
 };
