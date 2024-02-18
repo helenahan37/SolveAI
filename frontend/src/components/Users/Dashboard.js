2e0261dd187c47765894512dbccf7a85870f2d85
Helena.Han <hlyhelena37@gmail.com>
2024-02-19 01:23:38 +1100

    update styling

--- frontend/src/components/Users/Dashboard.js
+++ frontend/src/components/Users/Dashboard.js
@@ -18,7 +18,7 @@ const Dashboard = () => {
 			<div className="mb-6 bg-white p-4 shadow rounded-lg">
 				<h1 className="text-4xl font-bold text-center mb-12 text-white">Customer Dashboard</h1>
 
-				<div className="grid grid-cols-1 md:grid-cols-2 gap-6 m-6">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6 m-4">
 					{/* Profile Section */}
 					<div className="mb-6 bg-white p-4 shadow rounded-lg">
 						<h2 className="text-2xl font-semibold mb-6 text-white">Profile Information</h2>
