f29b95b116297c8385b6d0884ffd25a587e6bef4
Helena.Han <hlyhelena37@gmail.com>
2024-04-28 12:57:27 +1000

    update validation

--- frontend/src/components/Users/Register.js
+++ frontend/src/components/Users/Register.js
@@ -11,7 +11,7 @@ import { useAuth } from '../../AuthContext/AuthContext';
 // Validation schema
 const validationSchema = Yup.object({
 	email: Yup.string().email('Enter a valid email').required('Email is required'),
-	password: Yup.string().required('Password is required'),
+	password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
 	username: Yup.string().required('Username is required'),
 });
 
